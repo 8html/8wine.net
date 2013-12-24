be1f5f5e63da90615e96714ec22f4f5bf6827fcc
caiguanhao <caiguanhao@gmail.com>
2013-12-25 01:29:47 +0800

    better filter options;

--- assets/js/find.js
+++ assets/js/find.js
@@ -4,16 +4,30 @@ $(function(){
       itemSelector: '.filtered_item',
       onLayout: function() {
         $(window).trigger('scroll');
+        var total = $('.filtered_item').length;
+        var hidden = $('.isotope-hidden').length;
+        if (hidden === 0) {
+          $('#typeselector .all_listed').removeClass('hidden');
+          $('#typeselector .not_all_listed').addClass('hidden');
+          $('#typeselector a.showall').hide();
+        } else {
+          $('#typeselector .all_listed').addClass('hidden');
+          $('#typeselector .not_all_listed').removeClass('hidden');
+          $('#typeselector a.showall').show();
+        }
+        $('#typeselector .total_items').text(total);
+        $('#typeselector .visible_items').text(total - hidden);
       }
     });
-    $('#typeselector a[data-filter]').click(function(e) {
+    $('#typeselector a').click(function(e) {
       e.preventDefault();
-      if ($(this).data('filter') == '*') {
+      if (!$(this).data('filter')) {
         $('#typeselector a[data-filter]').removeClass('active');
+        $('#typeselector .valtd').each(function() {
+          $(this).find('a[data-filter]:first').addClass('active');
+        });
       } else {
-        if ($(this).hasClass('active')) {
-          $(this).removeClass('active');
-        } else {
+        if (!$(this).hasClass('active')) {
           $(this).siblings().removeClass('active');
           $(this).addClass('active');
         }
@@ -23,12 +37,6 @@ $(function(){
         var f=$(this).data('filter');
         if (filter.indexOf(f)===-1) filter+=f;
       });
-      if (!filter) {
-        filter = '*';
-        $('#typeselector a.showall').hide();
-      } else {
-        $('#typeselector a.showall').show();
-      }
       $('.filtered').isotope({
         filter: filter
       });
